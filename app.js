f1dc5cda07bda493e0836873d2ef2632b7f2223c
Paul <247144100+paulperrin-stack@users.noreply.github.com>
2026-04-01 14:16:10 +0700

    fix: use dynamic PORT for deployment

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const indexRouter = require("./routes/indexRouter");
 
 app.use("/", indexRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
